15315b7ff5c9a7693c63c369738f6d8b91817c4f
kadhaDeemi <dami.valderrama@duocuc.cl>
2025-07-23 11:21:14 -0400

    firts commit - Add API implementation for match data

---
+++ src/PartidoUChile.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+
+export default function PartidoUdeChile() {
+    const [racha, setRacha] = useState([]);
+    const [proximo, setProximo] = useState(null);
+
+
+    useEffect(() =>{
+        //datos simulados
+        
+    })
+
+
+
+
+}
