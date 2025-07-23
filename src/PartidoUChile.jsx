de6fd240b39f0509213f6259c44a53aeca9d0ae9
kadhaDeemi <dami.valderrama@duocuc.cl>
2025-07-23 11:37:51 -0400

    add estructura y datos simulados

--- src/PartidoUChile.jsx
+++ src/PartidoUChile.jsx
@@ -7,10 +7,49 @@ export default function PartidoUdeChile() {
 
     useEffect(() =>{
         //datos simulados
-        
-    })
+        const ultimosResultados = ["V", "V", "E", "V", "D"];
+        const siguientePartido = {
+            local : "Universidad de Chile",
+            visitante: "Coquimbo unido",
+            fecha : "2025-07-24T18:00:00Z"
+        };
 
+        setRacha(ultimosResultados);
+        setProximo(siguientePartido);
+    }, []);
 
+    const formatoFecha = (fechaISO) =>{
+        const fecha = new Date(fechaISO);
+        return fecha.toLocaleDateString( "es-CL", {
+            weekday : "long",
+            hour : "2-digit",
+            minute : "2-digit"
+        });
+    };
 
+    return (
+    <div style={{ 
+      border: "1px solid #ccc", 
+      borderRadius: "10px", 
+      padding: "20px", 
+      textAlign: "center", 
+      maxWidth: "300px",
+      margin: "50px auto",
+      background: "#f4f4f4",
+      color : 'black' 
+    }}>
+      <h2>Últimos 5 partidos</h2>
+      <p style={{ fontSize: "24px", fontWeight: "bold" }}>{racha.join(" - ")}</p>
 
+      {proximo && (
+        <div style={{ marginTop: "20px" }}>
+          <h3>Próximo partido:</h3>
+          <p style={{ fontSize: "18px" }}>
+            {proximo.local} vs {proximo.visitante}
+          </p>
+          <p>{formatoFecha(proximo.fecha)}</p>
+        </div>
+      )}
+    </div>
+  );
 }
